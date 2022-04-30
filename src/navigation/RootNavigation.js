7e9a3f068164a216f29278a939e4d865d2dae7be
Sunny Goswami <sungoswami07@gmail.com>
2022-04-30 18:21:28 +0530

    Profile screen UI completed

--- src/navigation/RootNavigation.js
+++ src/navigation/RootNavigation.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeTabs from './HomeTabs';
 import ChatScreen from '../screens/ChatScreen';
+import ProfileScreen from '../screens/ProfileScreen';
 
 const Stack = createStackNavigator();
 
@@ -10,6 +11,7 @@ const RootNavigation = () => {
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="Main" component={HomeTabs} />
       <Stack.Screen name="Chat" component={ChatScreen} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
     </Stack.Navigator>
   );
 };
